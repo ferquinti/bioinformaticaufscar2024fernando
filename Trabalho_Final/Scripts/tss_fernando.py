cac0b58518e42bc162a9345a39a6573efb98d098
ferquinti <168840006+ferquinti@users.noreply.github.com>
2024-05-31 02:01:03 -0300

    Delete Trabalho_Final/Scripts/tss_fernando.py

--- Trabalho_Final/Scripts/tss_fernando.py
+++
@@ -1,7 +0,0 @@
-#TSS
-def calcular_tss(tabela_original):
-    soma_colunas = tabela_original.sum(axis=0)
-    prop = tabela_original / soma_colunas
-    multiplicado = prop * 100000
-    inteiros = multiplicado.astype(int)
-    return inteiros
